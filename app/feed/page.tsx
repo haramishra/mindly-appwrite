65b90aa66c82122a3e6ba78ce33e1deb760eb3f4
haramishra <haraprasad45@gmail.com>
2023-05-26 01:11:11 +0530

    feed page added

---
+++ app/feed/page.tsx
@@ -0,0 +1,15 @@
+import { FeedPage } from "@/components/feedPage"
+import { SiteHeader } from "@/components/site-header"
+
+const Feed = async () => {
+  return (
+    <>
+      <SiteHeader />
+      <section className="container max-w-5xl grid items-center gap-6 pb-8 pt-6 md:py-10">
+        <FeedPage />
+      </section>
+    </>
+  )
+}
+
+export default Feed
