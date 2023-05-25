460c0f363117ff1a26a5da18b01ebcf6020b2b79
haramishra <haraprasad45@gmail.com>
2023-05-26 01:11:25 +0530

    new icons added

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,6 +1,9 @@
 import {
+  Bookmark,
   LucideProps,
+  MessageCircle,
   Moon,
+  MoreVertical,
   SunMedium,
   Twitter,
   type Icon as LucideIcon,
@@ -12,6 +15,9 @@ export const Icons = {
   sun: SunMedium,
   moon: Moon,
   twitter: Twitter,
+  bookmark: Bookmark,
+  comments: MessageCircle,
+  moreVertical: MoreVertical,
   logo: (props: LucideProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
       <path
