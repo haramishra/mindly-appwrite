301c932ce56f8b373be3953600ee3247012f9e59
haramishra <haraprasad45@gmail.com>
2023-05-25 18:10:42 +0530

    login pages added

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { Login } from "@/components/auth/login"
+
+export default function IndexPage() {
+  return (
+    <section className="grid items-center">
+      <Login />
+    </section>
+  )
+}
