ca170cef5bdbc57d356f4981ae7ff3edee2a1d12
haramishra <haraprasad45@gmail.com>
2023-05-25 18:10:54 +0530

    signup page added

---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { Register } from "@/components/auth/register"
+
+export default function IndexPage() {
+  return (
+    <section className="grid items-center ">
+      <Register />
+    </section>
+  )
+}
