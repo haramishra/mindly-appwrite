2897f33545360b669e03d199bc3e5812e5ff08e7
haramishra <haraprasad45@gmail.com>
2023-05-27 03:00:05 +0530

    reactNode type added

--- components/errorAlert.tsx
+++ components/errorAlert.tsx
@@ -1,12 +1,13 @@
+import { ReactNode } from "react"
 import { AlertCircle } from "lucide-react"
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 
 type AlertProps = {
-  children: string | null
+  children: string | null | ReactNode
 }
 
-export function AlertDestructive(props: AlertProps) {
+export function ErrorAlert(props: AlertProps) {
   return (
     <Alert variant="destructive">
       <AlertCircle className="h-4 w-4" />
