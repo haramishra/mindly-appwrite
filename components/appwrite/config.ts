8ae0e4f1c6e5bbb5471296679e7c0f1aeb6a09fd
haramishra <haraprasad45@gmail.com>
2023-05-27 01:02:42 +0530

    projectid env typo fixed

--- components/appwrite/config.ts
+++ components/appwrite/config.ts
@@ -2,9 +2,11 @@ import { Account, Client, Databases } from "appwrite"
 
 const client = new Client()
 
+console.log()
+
 client
   .setEndpoint(process.env.NEXT_PUBLIC_API_END_POINT)
-  .setProject(process.env.NEXT_PUBLIC_API_END_POINT)
+  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID)
 
 export const account = new Account(client)
 
