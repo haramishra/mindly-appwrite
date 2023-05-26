558871a7f2a282a6804c8923490125cf42210b3d
haramishra <haraprasad45@gmail.com>
2023-05-27 01:03:41 +0530

    right container added

--- components/feedPage/index.tsx
+++ components/feedPage/index.tsx
@@ -1,11 +1,13 @@
 import { PostsCard } from "./posts-card"
+import RightContainer from "./right-container"
 
 export function FeedPage() {
   return (
-    <>
-      <PostsCard />
-    </>
+    <div>
+      <div className="md:flex gap-12">
+        <PostsCard />
+        <RightContainer />
+      </div>
+    </div>
   )
 }
-
-
