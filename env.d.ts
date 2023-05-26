0bac5f232c955c265a35ae80794ac019f8e361fc
haramishra <haraprasad45@gmail.com>
2023-05-27 01:02:21 +0530

    projectid typo fixed

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_API_END_POINT: string
-    NEXT_PUBLIC_API_END_POINT: string
+    NEXT_PUBLIC_PROJECT_ID: string
     NEXT_PUBLIC_DATABASE_ID: string
   }
 }
