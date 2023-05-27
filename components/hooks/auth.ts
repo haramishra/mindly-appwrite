aa96431a17ec4b5090b8ca8d204ebd54fa67c72e
haramishra <haraprasad45@gmail.com>
2023-05-27 06:08:24 +0530

    hook for email signup

---
+++ components/hooks/auth.ts
@@ -0,0 +1,36 @@
+import { useCallback, useState } from "react"
+import { Account, ID } from "appwrite"
+
+import { EmailAndPasswordActionHook, UserObject } from "../types/auth"
+
+function UseCreateUserWithEmailPassword(
+  account: any
+): EmailAndPasswordActionHook {
+  const [user, setUser] = useState<undefined | UserObject>(undefined)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<undefined | string>(undefined)
+
+  const createUserWithEmailPassword = useCallback<
+    (email: string, password: string) => void
+  >(
+    (email: string, password: string) => {
+      setLoading(true)
+      const promise = account.create(ID.unique(), email, password)
+      promise.then(
+        function (res: UserObject | undefined) {
+          setUser(res) // Success
+          setLoading(false)
+        },
+        function (error: any) {
+          setError(error.message) // Failure
+          setLoading(false)
+        }
+      )
+    },
+    [account]
+  )
+
+  return [createUserWithEmailPassword, user, loading, error]
+}
+
+export default UseCreateUserWithEmailPassword
