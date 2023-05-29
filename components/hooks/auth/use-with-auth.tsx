f56e2788dc0c37fad9bdf9349c5c89a880f9e5b1
haramishra <haraprasad45@gmail.com>
2023-05-29 11:33:11 +0530

    with auth hook added

---
+++ components/hooks/auth/use-with-auth.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/navigation"
+
+import { account } from "@/components/appwrite/config"
+import useGetCurrentUser from "@/components/hooks/account/use-get-current-account-hook"
+
+function withAuth(ChildComponenet: React.FC) {
+  return function (props: any) {
+    const [user, loading, error] = useGetCurrentUser(account)
+    const router = useRouter()
+
+    if (loading) {
+      return <div>loading</div>
+    }
+
+    if (error || !user) {
+      router.replace("/login")
+    }
+
+    return <ChildComponenet {...props} user={user || undefined} />
+  }
+  return null
+}
+
+export default withAuth
