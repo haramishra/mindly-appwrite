69c3b86d030247c9a9fb31c4507fcf7c44986cdd
haramishra <haraprasad45@gmail.com>
2023-05-29 09:06:04 +0530

    test function

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,25 +1,29 @@
 "use client"
 
-import { account } from "@/components/appwrite/config"
+import { Button } from "@/components/ui/button"
+import { account, functions } from "@/components/appwrite/config"
 import { FeedPage } from "@/components/feedPage"
 import { SiteHeader } from "@/components/site-header"
 
 const Feed = () => {
-  const promise = account.get()
+  const testFunc = () => {
+    const promise = functions.createExecution("createUser", "hello")
 
-  promise.then(
-    function (response) {
-      console.log(response) // Success
-    },
-    function (error) {
-      console.log(error) // Failure
-    }
-  )
+    promise.then(
+      function (response) {
+        console.log(response) // Success
+      },
+      function (error) {
+        console.log(error) // Failure
+      }
+    )
+  }
   return (
     <>
       <SiteHeader />
       <section className="container max-w-5xl grid items-center gap-6 pb-8 pt-6 md:py-10">
         <FeedPage />
+        <Button onClick={testFunc}>click</Button>
       </section>
     </>
   )
