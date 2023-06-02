8f2586b24ba96f5c0af780fbf0ad1f0f859bc6be
haramishra <haraprasad45@gmail.com>
2023-06-03 03:10:39 +0530

    created bariabe for each ids

--- components/appwrite/config.ts
+++ components/appwrite/config.ts
@@ -2,14 +2,11 @@ import { Account, Client, Databases, Functions } from "appwrite"
 
 const client = new Client()
 
-console.log()
+export const APPWRITE_ENDPOINT = process.env.NEXT_PUBLIC_API_END_POINT
+export const APPWRITE_PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID
 
-client
-  .setEndpoint(process.env.NEXT_PUBLIC_API_END_POINT)
-  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID)
+client.setEndpoint(APPWRITE_ENDPOINT).setProject(APPWRITE_PROJECT_ID)
 
 export const account = new Account(client)
-
 export const database = new Databases(client)
-
 export const functions = new Functions(client)
