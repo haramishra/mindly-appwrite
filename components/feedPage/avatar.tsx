41c8d89a65aaf748fad34c786b8c124f69a4d2e2
haramishra <haraprasad45@gmail.com>
2023-06-04 02:58:04 +0530

    props added and made modular

--- components/feedPage/avatar.tsx
+++ components/feedPage/avatar.tsx
@@ -1,16 +1,32 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-function AvatarWithUID() {
+export function UserAvatar(props: { src: string; fallbackText: string }) {
+  return (
+    <Avatar>
+      <AvatarImage src={props.src} />
+      <AvatarFallback>{props.fallbackText}</AvatarFallback>
+    </Avatar>
+  )
+}
+
+function AvatarWithUID(props: {
+  name: string
+  subtext: string
+  image: string
+}) {
+  const placeholderImg =
+    "https://api.dicebear.com/6.x/lorelei/svg?seed=Spooky&backgroundType=gradientLinear&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf"
   return (
     <div>
       <div className="flex items-center space-x-4">
-        <Avatar>
-          <AvatarImage src="https://api.dicebear.com/6.x/lorelei/svg?seed=Spooky&backgroundType=gradientLinear&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf" />
-          <AvatarFallback>OM</AvatarFallback>
-        </Avatar>
+        <UserAvatar src={props.image ?? placeholderImg} fallbackText="om" />
         <div>
-          <p className="text-sm font-medium leading-none">Sofia Davis</p>
-          <p className="text-xs text-muted-foreground">6hrs ago</p>
+          <p className="text-sm font-medium leading-none">
+            {props.name ?? "Sofia Davis"}
+          </p>
+          <p className="text-xs text-muted-foreground">
+            {props.subtext ?? "6hrs ago"}
+          </p>
         </div>
       </div>
     </div>
