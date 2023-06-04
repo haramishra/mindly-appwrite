4e39ef2e52ce8d01c978575616a92e875131a087
haramishra <haraprasad45@gmail.com>
2023-06-05 02:49:42 +0530

    content-fit config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,6 +70,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      width: {
+        "w-fit": "fit-content",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
