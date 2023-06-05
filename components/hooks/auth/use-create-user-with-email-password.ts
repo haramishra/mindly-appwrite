2584b42e4cdf4484ecc042858b3876e97ca94658
haramishra <haraprasad45@gmail.com>
2023-06-06 02:03:25 +0530

    create user function execution added

--- components/hooks/auth/use-create-user-with-email-password.ts
+++ components/hooks/auth/use-create-user-with-email-password.ts
@@ -1,7 +1,9 @@
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { ID } from "appwrite"
 
 import { EmailAndPasswordActionHook, UserObject } from "@/types/auth"
+import { FunctionObject } from "@/types/function"
+import { functions } from "@/components/appwrite/config"
 
 function UseCreateUserWithEmailPassword(
   account: any
@@ -9,6 +11,22 @@ function UseCreateUserWithEmailPassword(
   const [user, setUser] = useState<undefined | UserObject>(undefined)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<undefined | string>(undefined)
+
+  useEffect(() => {
+    if (user) {
+      const promise = functions.createExecution("createUser")
+
+      promise.then(
+        function (response) {
+          console.log(response)
+          return response // Success
+        },
+        function (error) {
+          return error // Failure
+        }
+      )
+    }
+  }, [user])
 
   const createUserWithEmailPassword = useCallback<
     (email: string, password: string) => void
@@ -18,8 +36,18 @@ function UseCreateUserWithEmailPassword(
       const promise = account.create(ID.unique(), email, password)
       promise.then(
         function (res: UserObject | undefined) {
-          setUser(res) // Success
-          setLoading(false)
+          const promise = functions.createExecution("createUser")
+
+          promise.then(
+            function (response) {
+              console.log(response)
+              setUser(res) // Success
+              setLoading(false)
+            },
+            function (error) {
+              setError(error.message)
+            }
+          )
         },
         function (error: any) {
           setError(error.message) // Failure
