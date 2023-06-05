906183f953fc716ea56f17ef5117d9b78ee1206d
haramishra <haraprasad45@gmail.com>
2023-06-06 04:00:22 +0530

    add post form with logic added

--- components/feedPage/addPost.tsx
+++ components/feedPage/addPost.tsx
@@ -1,10 +1,14 @@
 "use client"
 
 import { useRef, useState } from "react"
+import { useRouter } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { ID } from "appwrite"
+import { Check, ChevronsUpDown } from "lucide-react"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 
+import { cn } from "@/lib/utils"
 import {
   Form,
   FormControl,
@@ -15,11 +19,31 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Textarea } from "@/components/ui/textarea"
+import { account, database } from "@/components/appwrite/config"
 
 import useAutosizeTextArea from "../hooks/ui/use-auto-height-textarea"
 import { Button } from "../ui/button"
+import { Checkbox } from "../ui/checkbox"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "../ui/command"
 import { Input } from "../ui/input"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
 import { UserAvatar } from "./avatar"
+
+const tags = [
+  { label: "Tips", value: "tips" },
+  { label: "Vent", value: "vent" },
+  { label: "Advice", value: "advice" },
+  { label: "Help", value: "help" },
+  { label: "Question", value: "question" },
+  { label: "SOS", value: "sos" },
+  { label: "Other", value: "other" },
+] as const
 
 const FormSchema = z.object({
   content: z
@@ -38,20 +62,61 @@ const FormSchema = z.object({
     .max(200, {
       message: "title must not be longer than 200 characters",
     }),
+  nsfw: z.boolean().default(false).optional(),
+  tag: z.string({
+    required_error: "Please select a tag.",
+  }),
 })
 function AddPost() {
   const [showInputs, setShowInputs] = useState(false)
+
+  const router = useRouter()
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       content: "",
       title: "",
+      nsfw: false,
+      tag: "",
     },
   })
+  const cleanup = () => {
+    form.reset()
+    setShowInputs(false)
+  }
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log("submit")
+  function onSubmit(values: z.infer<typeof FormSchema>) {
+    const accPromise = account.get()
+    accPromise
+      .then((result) => {
+        const body = {
+          content: values.content,
+          title: values.title,
+          userId: result.$id,
+          name: result.name,
+          nsfw: values.nsfw,
+        }
+        console.log(body)
+        const dbPromise = database.createDocument(
+          "6469db4675eba906b2ff",
+          "6469db77d5fa468db513",
+          ID.unique(),
+          body
+        )
+
+        dbPromise.then(
+          function (response) {
+            console.log(response) // Success
+            cleanup()
+            router.refresh()
+          },
+          function (error) {
+            console.log(error) // Failure
+          }
+        )
+      })
+      .catch((err) => {})
   }
 
   const textAreaRef = useRef<HTMLTextAreaElement>(null)
@@ -115,6 +180,93 @@ function AddPost() {
                       }/1000`}</FormDescription>
                     )}
                     <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className={`${showInputs ? "block" : "hidden"}`}>
+              <FormField
+                control={form.control}
+                name="tag"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    {/* <FormLabel>Tags</FormLabel> */}
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant="outline"
+                            role="combobox"
+                            className={cn(
+                              "w-[200px] justify-between",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value
+                              ? tags.find((tag) => tag.value === field.value)
+                                  ?.label
+                              : "Select a tag"}
+                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-[200px] p-0">
+                        <Command>
+                          <CommandInput placeholder="Search framework..." />
+                          <CommandEmpty>No tags found.</CommandEmpty>
+                          <CommandGroup>
+                            {tags.map((tag) => (
+                              <CommandItem
+                                value={tag.value}
+                                key={tag.value}
+                                onSelect={(value) => {
+                                  form.setValue("tag", value)
+                                }}
+                              >
+                                <Check
+                                  className={cn(
+                                    "mr-2 h-4 w-4",
+                                    tag.value === field.value
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                                {tag.label}
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                    {/* <FormDescription>
+                      Select one Tag for your post
+                    </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className={`${showInputs ? "block" : "hidden"}`}>
+              <FormField
+                control={form.control}
+                name="nsfw"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-start space-x-3 space-y-0  ">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <div className="space-y-1 leading-none">
+                      <FormLabel>This post contains NSFW.</FormLabel>
+                      <FormDescription>
+                        Please check the box if the post contains explicit or
+                        triggering elements
+                      </FormDescription>
+                    </div>
                   </FormItem>
                 )}
               />
