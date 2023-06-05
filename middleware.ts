ea7bfc161a843167d2cb66ddfd4c64e9864b22fb
haramishra <haraprasad45@gmail.com>
2023-06-06 01:41:56 +0530

    route protection for auth

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
 import { NextRequest, NextResponse } from "next/server"
 
+//check for login and signup route
+const isAuthRoute = (pathname: string) => {
+  return pathname.startsWith("/login") || pathname.startsWith("/signup")
+}
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const session = request.cookies.get("auth-session")
-  console.log(!session?.value)
+  const { pathname } = request.nextUrl
+
   if (!session?.value && request.url) {
+    if (isAuthRoute(pathname)) {
+      return NextResponse.next()
+    }
     return NextResponse.redirect(new URL("/", request.url))
   }
 
@@ -12,5 +21,10 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/feed/:path*', '/profile/:path*']
-};
+  matcher: [
+    "/feed/:path*",
+    "/profile/:path*",
+    "/login/:path*",
+    "/signup/:path*",
+  ],
+}
