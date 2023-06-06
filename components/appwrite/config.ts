6ecf84dc6dfde884c5fdab71d615ecaaf84b9541
haramishra <haraprasad45@gmail.com>
2023-06-06 19:47:56 +0530

    appwrite variables added

--- components/appwrite/config.ts
+++ components/appwrite/config.ts
@@ -5,6 +5,12 @@ const client = new Client()
 export const APPWRITE_ENDPOINT = process.env.NEXT_PUBLIC_API_END_POINT
 export const APPWRITE_PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID
 
+export const MINDLY_DB_DATABASE_ID = "6469db4675eba906b2ff"
+export const USER_COLLECTION_ID = "6469db66cffc8f1ac755"
+export const POSTS_COLLECTION_ID = "6469db77d5fa468db513"
+export const COMMENTS_COLLECTION_ID = "646a2f3439f58b8b62b0"
+export const SAVED_POSTS_COLLECTION_ID = "6469db96389d310baaca"
+
 client.setEndpoint(APPWRITE_ENDPOINT).setProject(APPWRITE_PROJECT_ID)
 
 export const account = new Account(client)
