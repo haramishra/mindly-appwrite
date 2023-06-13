42e62143f739a5ec2473b7622b8ad39c535eb8fc
haramishra <haraprasad45@gmail.com>
2023-06-13 14:06:36 +0530

    content container for profile

---
+++ components/profiles/content-container.tsx
@@ -0,0 +1,25 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+import UserPosts from "./posts"
+
+function ContentContainer() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Tabs
+        defaultValue="saved"
+        className="flex flex-col justify-center items-center"
+      >
+        <TabsList>
+          <TabsTrigger value="saved">Saved Posts</TabsTrigger>
+          <TabsTrigger value="posts">My Posts</TabsTrigger>
+        </TabsList>
+        <TabsContent value="saved">saved content</TabsContent>
+        <TabsContent value="posts">
+          <UserPosts />
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
+}
+
+export default ContentContainer
