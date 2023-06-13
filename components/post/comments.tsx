159352c21e6b772adb8e3b1a0855f7c1efe17bbc
haramishra <haraprasad45@gmail.com>
2023-06-13 14:06:26 +0530

    query added

--- components/post/comments.tsx
+++ components/post/comments.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react"
+import { Query } from "appwrite"
+
 import {
   COMMENTS_COLLECTION_ID,
   MINDLY_DB_DATABASE_ID,
@@ -5,51 +8,67 @@ import {
 } from "../appwrite/config"
 import { AddCommnet } from "./add-comments"
 import Comment from "./comment"
+import { CommentsLoader } from "./loader"
 
 function CommentsContainer({ postId }: { postId: string }) {
-  const comments: any = [
-    // {
-    //   $id: "dslfj",
-    //   userID: "sldfj",
-    //   userName: "pipy_pily",
-    //   avatarFallback: "pp",
-    //   content: "this is a demo comment for ui usages",
-    //   time: "4min ago",
-    // },
-    // {
-    //   $id: "dsledfj",
-    //   userID: "sldfjsdf",
-    //   userName: "pihey_sdfily",
-    //   avatarFallback: "ps",
-    //   content:
-    //     " this is a demo comment for ui usages this is a demo comment for ui usages this is a demo comment for ui usages this is a demo comment for ui usages this is a demo comment for ui usages",
-    //   time: "4min ago",
-    // },
-    // {
-    //   $id: "dsfeslfj",
-    //   userID: "sldecfj",
-    //   userName: "pshw_slien",
-    //   avatarFallback: "ps",
-    //   content:
-    //     "this is a demo comment for ui usages this is a demo comment for ui usages",
-    //   time: "4min ago",
-    // },
+  const Democomments: any = [
+    {
+      $id: "dslfj",
+      userID: "sldfj",
+      userName: "pipy_pily",
+      avatarFallback: "pp",
+      content: "this is a demo comment for ui usages",
+      time: "4min ago",
+    },
+    {
+      $id: "dsledfj",
+      userID: "sldfjsdf",
+      userName: "pihey_sdfily",
+      avatarFallback: "ps",
+      content:
+        " this is a demo comment for ui usages this is a demo comment for ui usages this is a demo comment for ui usages this is a demo comment for ui usages this is a demo comment for ui usages",
+      time: "4min ago",
+    },
+    {
+      $id: "dsfeslfj",
+      userID: "sldecfj",
+      userName: "pshw_slien",
+      avatarFallback: "ps",
+      content:
+        "this is a demo comment for ui usages this is a demo comment for ui usages",
+      time: "4min ago",
+    },
   ]
 
-  const getComments = () => {
+  const [comments, setComments] = useState<any>([])
+  const [loading, setLoading] = useState(false)
+
+  const getComments = (postId: string) => {
+    setLoading(true)
     const getCommentsPromise = database.listDocuments(
       MINDLY_DB_DATABASE_ID,
-      COMMENTS_COLLECTION_ID
+      COMMENTS_COLLECTION_ID,
+      [Query.equal("postId", postId), Query.orderDesc("$updatedAt")]
     )
 
     getCommentsPromise.then(
       function (response) {
+        setLoading(false)
         console.log(response) // Success
       },
       function (error) {
+        setLoading(false)
         console.log(error) // Failure
       }
     )
+  }
+
+  useEffect(() => {
+    getComments(postId)
+  }, [postId])
+
+  if (loading) {
+    return <CommentsLoader />
   }
 
   return (
@@ -59,7 +78,7 @@ function CommentsContainer({ postId }: { postId: string }) {
         postID={postId}
       />
 
-      {comments.length ? (
+      {Democomments.length ? (
         <div className="mt-10">
           {comments.map((item: any) => (
             <div className="mb-6" key={item.$id}>
