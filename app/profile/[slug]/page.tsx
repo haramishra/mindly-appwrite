fe21145bc060667c922a92db3361e5eb22aecbbc
haramishra <haraprasad45@gmail.com>
2023-06-13 14:05:59 +0530

    view profile added

---
+++ app/profile/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import { FeedPage } from "@/components/feedPage"
+import { SiteHeader } from "@/components/site-header"
+
+const UserProfile = () => {
+  return (
+    <>
+      <SiteHeader />
+      <section className="container max-w-5xl grid items-center gap-6 pb-8 pt-6 md:py-10"></section>
+    </>
+  )
+}
+
+export default UserProfile
