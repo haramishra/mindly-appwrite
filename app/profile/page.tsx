42b26ff18645d7b5c6d4224edecbd20d571f0ad0
haramishra <haraprasad45@gmail.com>
2023-06-13 14:05:31 +0530

    profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,16 @@
+import { FeedPage } from "@/components/feedPage"
+import UserProfile from "@/components/profiles"
+import { SiteHeader } from "@/components/site-header"
+
+const Profile = () => {
+  return (
+    <>
+      <SiteHeader />
+      <section className="container max-w-5xl grid items-center gap-6 pb-8 pt-6 md:py-10">
+        <UserProfile />
+      </section>
+    </>
+  )
+}
+
+export default Profile
