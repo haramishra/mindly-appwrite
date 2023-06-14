b8f307ff1ada3c335256e44b6b602853152692dd
haramishra <haraprasad45@gmail.com>
2023-06-14 13:13:27 +0530

    current user added

--- components/profiles/index.tsx
+++ components/profiles/index.tsx
@@ -1,12 +1,21 @@
 "use client"
 
+import { account } from "../appwrite/config"
+import useGetCurrentUser from "../hooks/account/use-get-current-account-hook"
 import ContentContainer from "./content-container"
 import ProfileCard from "./profile-card"
+import ProfileLoader from "./profile-loader"
 
 function UserProfile() {
+  const [user, loading, error] = useGetCurrentUser(account)
+
+  if (!user || loading) {
+    return <ProfileLoader />
+  }
+
   return (
     <div>
-      <ProfileCard />
+      <ProfileCard user={user} />
       <ContentContainer />
     </div>
   )
