c52f7f0ed3efe1e1236c4fb3f8b7e8f7569343af
haramishra <haraprasad45@gmail.com>
2023-06-14 12:17:03 +0530

    tags and nsfw tag is added

--- components/feedPage/posts-content.tsx
+++ components/feedPage/posts-content.tsx
@@ -1,23 +1,30 @@
 import { Badge } from "@/components/ui/badge"
 
-function PostCardContent(props: { content: string; title: string }) {
+function PostCardContent(props: {
+  content: string
+  title: string
+  nsfw: boolean
+  tag: string
+}) {
   return (
     <div>
       <div className="mb-4">
         <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
           {props.title}
           <span className="ml-4 space-x-3">
-            <Badge
-              className="text-sm  font-normal border-red-700 text-red-700"
-              variant="outline"
-            >
-              NSFW
-            </Badge>
+            {props.nsfw && (
+              <Badge
+                className="text-sm  font-normal border-red-700 text-red-700"
+                variant="outline"
+              >
+                NSFW
+              </Badge>
+            )}
             <Badge
               className="text-sm font-normal dark:border-slate-400 light:border-slate-600"
               variant="outline"
             >
-              Tips
+              {props.tag}
             </Badge>
           </span>
         </h3>
