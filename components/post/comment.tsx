c95e642c942b0669abccc83b8ec3f060b6407ac0
haramishra <haraprasad45@gmail.com>
2023-06-14 23:47:20 +0530

    avatar added

--- components/post/comment.tsx
+++ components/post/comment.tsx
@@ -1,11 +1,13 @@
 import { UserAvatar } from "../feedPage/avatar"
+import { useGetAvatar } from "../hooks/account/use-get-avatar"
 import { Icons } from "../icons"
 
 function Comment({ comment }: { comment: any }) {
+  const avatar = useGetAvatar(comment.userId)
   return (
     <>
       <div className="flex  gap-4">
-        <UserAvatar src="" fallbackText={comment.avatarFallback} />
+        <UserAvatar src={avatar} fallbackText={""} />
 
         <div className=" gap-4  justify-between items-center rounded-lg p-4 pt-0">
           <div className="flex gap-3  w-4/5 justify-between">
