24f9fd6c66800a213b0881ffd0d0f32fb1d99f60
haramishra <haraprasad45@gmail.com>
2023-06-14 13:13:11 +0530

    profile card added

--- components/profiles/profile-card.tsx
+++ components/profiles/profile-card.tsx
@@ -1,12 +1,17 @@
+import { UserObject } from "@/types/auth"
+
+import { useGetAvatar } from "../hooks/account/use-get-avatar"
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 import { Separator } from "../ui/separator"
 
-function ProfileCard(props: any) {
+function ProfileCard({ user }: { user: UserObject | undefined }) {
+  const avatar = useGetAvatar(user?.$id || "")
+
   return (
     <div className="flex flex-col justify-center items-center">
       <Avatar className="h-40 w-40">
-        <AvatarImage src={props.src || ""} />
-        <AvatarFallback>{props.fallbackText || "hp"}</AvatarFallback>
+        <AvatarImage src={avatar || ""} />
+        <AvatarFallback>{""}</AvatarFallback>
       </Avatar>
 
       <h1 className="pt-5 text-3xl font-semibold">pink_pipp</h1>
